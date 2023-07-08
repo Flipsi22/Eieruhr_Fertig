9449e966dc9b799397bd348e46105f885b936891
Flipsi22 <137618566+Flipsi22@users.noreply.github.com>
2023-07-08 18:54:18 +0200

    Update WebServerHandler.cpp

--- WebServerHandler.cpp
+++ WebServerHandler.cpp
@@ -8,13 +8,6 @@ NTPClient timeClient(ntpUDP, "de.pool.ntp.org");
 
 AsyncWebServer server(80);
 
-extern Adafruit_NeoPixel strip;
-const int NUM_LEDS = 8;
-
-int targetValue = 0; // Target value received from the webpage
-int currentValue = 0; // Current value for the Neopixel animation
-unsigned long startTime = 0; // Start time of the timer
-
 void initWebServer() {
   timeClient.setTimeOffset(7200); // Zeitversatz für Mitteleuropäische Zeit (MEZ)
   timeClient.begin();
@@ -35,74 +28,54 @@ void initWebServer() {
       color: #333333;
     }
 
-    label {
+    .timer-container {
+      margin: 20px auto;
+      width: 300px;
+      background-color: #ffffff;
+      border-radius: 10px;
+      padding: 20px;
+      box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    }
+
+    .timer-value {
+      font-size: 36px;
       font-weight: bold;
+      margin-bottom: 20px;
+      color: #333333;
     }
 
-    input[type="number"] {
-      padding: 5px;
-      width: 100px;
-    }
-
-    button {
-      padding: 8px 16px;
-      background-color: #4CAF50;
-      color: #ffffff;
-      border: none;
-      cursor: pointer;
-      margin-top: 10px;
-    }
-
-    button:hover {
-      background-color: #45a049;
-    }
-
-    p {
-      margin-top: 20px;
-    }
-
-    #currentTime {
-      font-size: 24px;
-      font-weight: bold;
+    .timer-label {
+      font-size: 18px;
+      color: #666666;
     }
   </style>
-
-  <script>
-    function setTargetValue() {
-      var targetValue = document.getElementById("targetValue").value;
-      var xhr = new XMLHttpRequest();
-      xhr.open("GET", "/setTargetValue?value=" + targetValue, true);
-      xhr.send();
-    }
-
-    function startTimer() {
-      var xhr = new XMLHttpRequest();
-      xhr.open("GET", "/startTimer", true);
-      xhr.send();
-    }
-
-    function updateTime() {
-      var xhr = new XMLHttpRequest();
-      xhr.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-          var time = this.responseText;
-          document.getElementById("currentTime").textContent = time;
-        }
-      };
-      xhr.open("GET", "/getTime", true);
-      xhr.send();
-    }
-
-    setInterval(updateTime, 1000);
-  </script>
 </head>
 <body>
   <h1>Online abrufbarer Timer</h1>
-  <label for="targetValue">Zeit auswählen (in seconds):</label>
-  <input type="number" id="targetValue" name="targetValue">
-  <button onclick="setTargetValue()">Set</button>
-  <button onclick="startTimer()">Start</button>
-  <p>Restliche Zeit: <span id="currentTime"></span></p>
+
+  <div class="timer-container">
+    <p class="timer-value" id="currentMinutes"></p>
+    <p class="timer-label">Minuten</p>
+    <p class="timer-value" id="currentSeconds"></p>
+    <p class="timer-label">Sekunden</p>
+  </div>
+
+  <script>
+    setInterval(getRemainingTime, 1000);
+
+    function getRemainingTime() {
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          var response = this.responseText.split(":");
+          document.getElementById("currentMinutes").innerHTML = response[0];
+          document.getElementById("currentSeconds").innerHTML = response[1];
+        }
+      };
+      xhttp.open("POST", "/zeit", true);
+      xhttp.send();
+    }
+  </script>
 </body>
 </html>
 )rawliteral";
@@ -110,24 +83,12 @@ void initWebServer() {
     request->send(200, "text/html", html);
   });
 
-  server.on("/setTargetValue", HTTP_GET, [](AsyncWebServerRequest* request){
-    String valueStr = request->getParam("value")->value();
-    targetValue = valueStr.toInt();
-    request->send(200, "text/plain", "OK");
-  });
-
-  server.on("/startTimer", HTTP_GET, [](AsyncWebServerRequest* request){
-    startTime = millis();
-    currentValue = targetValue;
-    request->send(200, "text/plain", "OK");
-  });
-
-  server.on("/getTime", HTTP_GET, [](AsyncWebServerRequest* request){
-    unsigned long currentTime = millis();
-    unsigned long elapsedTime = (currentTime >= startTime) ? (currentTime - startTime) : 0;
-    unsigned long remainingTime = (elapsedTime >= targetValue * 1000) ? 0 : (targetValue * 1000 - elapsedTime);
-    String time = String(remainingTime / 1000);
-    request->send(200, "text/plain", time);
+  server.on("/zeit", HTTP_POST, [](AsyncWebServerRequest* request){
+    
+    unsigned long minutes = 10;
+    unsigned long seconds = 50;
+    String response = String(minutes) + ":" + String(seconds);
+    request->send(200, "text/plain", response);
   });
 
   server.begin();
@@ -135,49 +96,5 @@ void initWebServer() {
 
 void handleWebServer(unsigned long remainingTime) {
   timeClient.update();
-
-  // Neopixel animation
-  if (currentValue > 0) {
-    currentValue--;
-  }
-
-  int red = 0;
-  int green = 0;
-  int blue = 0;
-  
-  int ledIndex = (targetValue > 0) ? NUM_LEDS - (remainingTime / (targetValue * 1000 / NUM_LEDS)) : 0;
-  if (ledIndex >= NUM_LEDS) {
-    ledIndex = NUM_LEDS - 1;
-  }
-  
-  // Prüfen, ob der Timer gestartet wurde
-  if (targetValue > 0 && ledIndex >= 0) {
-    if (ledIndex < 2) {
-      red = 255;
-    } else if (ledIndex < 6) {
-      red = 255;
-      green = 165; // Orange
-    } else {
-      green = 255;
-    }
-  }
-  for (int i = 0; i < NUM_LEDS; i++) {
-    if (i <= ledIndex) {
-      strip.setPixelColor(i, red, green, blue);
-    } else {
-      strip.setPixelColor(i, 0, 0, 0); // Ausschalten der nicht aktivierten LEDs
-    }
-  }
-  strip.show();
-}
-
-
-void updateTimer() {
-  if (startTime > 0) {
-    unsigned long currentTime = millis();
-    unsigned long elapsedTime = (currentTime >= startTime) ? (currentTime - startTime) : 0;
-    unsigned long remainingTime = (elapsedTime >= targetValue * 1000) ? 0 : (targetValue * 1000 - elapsedTime);
-    currentValue = remainingTime / 1000;
-    handleWebServer(remainingTime);
-  }
+  // Keine Neopixel-Animation oder andere Funktionen erforderlich
 }
